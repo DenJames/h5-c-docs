6f19a67a644a473348d0b43e7ad76d5c695457f5
Dennis <83814476+DenJames@users.noreply.github.com>
2024-04-23 16:15:14 +0200

    feat: credits

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'H5 Docs',
+  tagline: 'Oliver & Dennis - Api Documentation H5',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
